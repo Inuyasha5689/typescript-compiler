16ea5c68bb57692532a650d525b4cde15852e686
Drake Scott <dascott0204@gmail.com>
2017-03-09 18:25:52 -0600

    the compiler got smarter with typescript 2.0 and can now tell when something is defined but never used among other new things.

--- app.ts
+++ app.ts
@@ -3,7 +3,16 @@
  */
 let myName: string = "Drake";
 let myAge: number = 20;
-let anything;
+let anything: any;
 anything = 12;
 
 // myName = 30;
+
+function controlMe(isTrue: boolean, somethingElse: boolean) {
+    let result: number;
+    if (isTrue) {
+        result = 12;
+    }
+    result = 33;
+    return result;
+}
